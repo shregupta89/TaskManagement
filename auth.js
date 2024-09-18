6a965a06b3ca4c0fb38d0943a95e475fcac7f2e1
shregupta89 <shregupta89@gmail.com>
2024-09-18 20:42:30 +0530

    added a few restrctions pt1

---
+++ auth.js
@@ -0,0 +1,20 @@
+const jwt=require("jsonwebtoken")
+const JWT_SECRET="jwtsecret"
+
+function auth(req,res,next){
+    const token =req.headers.token;
+    const decodedData=jwt.verify(token,JWT_SECRET)
+    if(decodedData){
+        req.userId=decodedData.id;
+        next();
+    }else{
+        res.status(403).json({
+            message:"Incorrect Creds"
+        })
+    }
+}
+
+module.exports={
+    auth,
+    JWT_SECRET
+}
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const {UserModel,TodoModel} = require("./db")
+const bcrypt=require("bcrypt")
+const{JWT_SECRET,auth}=require("./auth")
 const express= require("express")
-const jwt=require("jsonwebtoken")
-const JWT_SECRET="jwtsecret"
+const {z}=require("zod")
 
 const app=express()
 app.use(express.json())
 
-// mongodb+srv://admin:Shreya%402004@cluster0.dnrt8.mongodb.net/
+
 const mongoose=require("mongoose")
-mongoose.set('debug', true);
-const run = async () => {
-    try {
-      await mongoose.connect(
-        "mongodb+srv://admin:Shreya%402004@cluster0.dnrt8.mongodb.net/todo-app-database"
-      );
-      console.log("Connected to myDB");
-    } catch (err) {
-      console.error("Error connecting to MongoDB:", err);
-    }
-  };
-  
-  run(); 
+const connectionString = 'mongodb+srv://admin:Shreya%402004@cluster0.dnrt8.mongodb.net/todo-app-database'; // Replace with your MongoDB URI
+const options = {
+  connectTimeoutMS: 30000, // 30 seconds
+};
+
+mongoose.connect(connectionString, options)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 // mongoose.connect("mongodb+srv://admin:Shreya%402004@cluster0.dnrt8.mongodb.net/todo-app-database")
 //the name of the db is added at the end of the connection string 
@@ -32,13 +32,31 @@ app.post("/signup",async function(req,res){
     const email=req.body.email;
     const password=req.body.password;
     const name=req.body.name;
+    const requiredbody=z.object({
+        email:z.string().min(3).max(100).email(),
+        passowrd:z.string().min(8).max(15),
+        name:z.string().min(3).max(20)
+    })
 
-//if i do not await here and disconnect the mongoose library , i will still get "you are signed up" even though this task was not completed yet ,isliye ye task complete hue bina age nhi bdhna chahiye
-    await UserModel.create({
+    const pasrsedDataWithSuccess=requiredbody.safeParse(req.body); //req.body is the input body 
+    if(!pasrsedDataWithSuccess.success){
+        res.json({
+            message:"Incorrect format",
+            error:pasrsedDataWithSuccess.error
+        })
+        return 
+    }
+    const hashedPassword=await bcrypt.hash(password,5); 
+    //its doing smth expensive that is why have to await it 
+    //5 is the number of rounds that u eant to hash the password , the hgiher the number the number the harder it is to brute force it and more difficult to computationally run so ensure to have a intermediate number 
+
+    //if i do not await here and disconnect the mongoose library , i will still get "you are signed up" even though this task was not completed yet ,isliye ye task complete hue bina age nhi bdhna chahiye
+    await UserModel.create({ 
         email:email,
         password:password,
         name:name
     });
+   
     res.json({
         message:"You are signed up"
     })
@@ -53,9 +71,14 @@ app.post("/signin", async function(req,res){
 
     const response=await UserModel.findOne({   //all db calls have to be awaited 
         email:email,
-        password:password
     })
-    if(response){
+    if(!response){
+        res.status(403).json({
+            message:"You are not signed up"
+        })
+    }
+    const matchedPassword=await bcrypt.compare(password,response.password);
+    if(matchedPassword){
         const token =jwt.sign({
             id:response._id.toString() // encoding an object is undefined , have to convert to a string before
         },JWT_SECRET)
@@ -70,21 +93,31 @@ app.post("/signin", async function(req,res){
     }
 
 })
+app.post("/todo",auth,async function(req,res,next){
+    const userId=req.userId;
+    const title=req.body.title;
+    const done=req.body.done;
 
-// function auth(req,res,next){
-//     const token =req.headers.token;
-//     const decodedData=jwt.verify(token,JWT_SECRET)
-//     if(decodedData){
-//         req.userId=decodedData.id;
-//         next();
-//     }else{
-//         res.status(403).json({
-//             message:"Incorrect Creds"
-//         })
-//     }
-// }
-// app.post("/todo",function(req,res,next){
+    await TodoModel.create({
+        title,
+        done,
+        userId
+    })
+    res.json({
+        message:"Todo created"
+    })    
+} 
+)
+app.get("/todos",auth,async function(req,res,next){
+    const userId=req.userId;
+    const todos=await TodoModel.find({
+        userId:userId
+    })
+    res.json({
+        todos
+    })
+
+})
 
 
-// })
 app.listen(3000);
